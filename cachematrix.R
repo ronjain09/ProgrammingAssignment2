f2762747af38606960809e4bbb61a75ae7b0c0ca
ronjain09 <ronjain09@gmail.com>
2016-04-18 20:47:08 +0530

    Updated the code file cachematrix.R
    I have updated the code as per my best knowledge in R programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The code is written by Rahul Shri Shri ShriMal
+## The code is two simple functions makeCacheMatrix and cacheSolve which will help to calculate the inverse of a invertable matrix
+## Created on : 18th April 2016 2045 IST
 
-## Write a short comment describing this function
+
+## The function gets the value of the matrix and sets to a new matrix and then upon calling it validtes the inverse and returns the results
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+		## this function assigns the matrix
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+		## this function will fetch the matrix which called upon
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m ## this is the inverse of the matrix provided
 }
+    ## Return a matrix that is the inverse of 'x'
